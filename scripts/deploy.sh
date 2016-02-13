ea47aa04f7890045d75c2f79d300072b8f722ec9
Per Lundberg <perlun@gmail.com>
2016-02-13 23:16:25 +0200

    More tweaks trying to get the automagic deployments wokring.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-ssh-add -D
-ssh-add .deploy_key
-
 IP=85.134.56.45
 
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
 fi
 
-rsync -e 'ssh -oBatchMode=yes' -gloprtv --delete _site/ www-data@$IP:www.halleluja.nu
+rsync -e 'ssh -oBatchMode=yes' -gloprtv --delete ./_site/ www-data@$IP:www.halleluja.nu
